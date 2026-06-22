1e795a74dc9373cb256d5dab6b61690eb2e2c17a
mizutanitakahiro <mizutanitakahiro@mizutanitakahironoMacBook-Air.local>
2026-06-22 20:08:32 +0900

    Update PWA app icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,11 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: "/icons/tsurilog-icon-192.png", sizes: "192x192", type: "image/png" },
       { url: "/icons/tsurilog-icon.png", sizes: "512x512", type: "image/png" }
     ],
     apple: [
-      { url: "/icons/tsurilog-icon.png", sizes: "512x512", type: "image/png" }
+      { url: "/icons/apple-touch-icon.png", sizes: "180x180", type: "image/png" }
     ]
   },
   appleWebApp: {
